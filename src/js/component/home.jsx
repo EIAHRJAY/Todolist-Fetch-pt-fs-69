a22e0bda6921f2e5b185195a49e8b48a6284b396
Eduardo Ribeiro <166298206+EIAHRJAY@users.noreply.github.com>
2024-06-23 01:21:17 +0000

    update

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -7,10 +7,11 @@ const Home = () => {
 useEffect(() =>{
     getTasks();
   }, []);
+ 
   //sincronizando api
 async function getTasks() {
   try{
-    const response = await fetch("https://playground.4geeks.com/todo/users/EHIARJAY");
+    const response = await fetch("https://playground.4geeks.com/todo/users/EIAHRJAY");
     if(!response.ok){
       throw new Error(`Error : ${response.status}`);
     }
@@ -20,10 +21,11 @@ async function getTasks() {
     console.error("Failed to fetch tasks",error);
   }
 }
+
 // adicionar tarefas
 async function addTodos(){
 try{
-  const newTodo = { label: inputValue};
+  const newTodo = { label: inputValue, is_done: false};
   const response = await fetch("https://playground.4geeks.com/todo/todos/EIAHRJAY",{
     method: "POST",
     headers: {
@@ -34,13 +36,61 @@ try{
   if(!response.ok){
     throw new Error(`Error: ${response.status}`)
   }
-  const data = await response.json();
-  setTodos([...todos, data.todo]);
-  setInputValue("");
+  
+   const data = await response.json();
+  console.log('Added todo:',data);
+  
+  if(data && data.id) {
+    setTodos([...todos, data.todo]);
+    setInputValue("");
+    await updateTodos(data.id, inputValue);
+  }else {
+    throw new Error("Error: ${response.status}");
+  }
 }catch(error){
   console.error("Failed to add todo:", error);
 }
 }
+
+//DELETE tarea
+ async function deleteTodo(id){
+   try{
+     const response = await fetch (`https://playground.4geeks.com/todo/todos/${id}`,{
+      method: "DELETE",
+      headers: {
+        "Content-Type" : "application/json"
+      },
+    });
+    if (!response.ok){
+      throw new Error(`Error: ${response.status}`);    }
+     setTodos(todos.filter(todo => todo.id !== id));
+  }catch(error){
+    console.error("Faleid to delete todo:",error);
+  }
+ }
+
+ //ATUALIZAR DATOS
+async function updateTodos(id, updateLabel){
+  try{
+    const updateTodo = {label: updateLabel, is_done: true };
+    const response = await fetch(`https://playground.4geeks.com/todo/todos/${id}`, {
+      method: "PUT",
+      headers:{
+        "Content-Type" : "ap[lication/json"
+      },
+      body: JSON.stringify(updateTodo),
+    });
+    if (!response.ok){
+      throw new Error(`Error: ${response.status}`);
+    }
+    const data = await response.json();
+    setTodos(todos.map(todo => (todo.id === id ? data : todo)));
+  }catch(error){
+    console.error("failed to update todo: ", error);
+  }
+}
+
+
 
 
 
@@ -48,28 +98,25 @@ try{
     <div className="container">
       <h1>Todos </h1>
       <div>
-      <ul>
-        <li ><input
+       <ul>
+        <li >
+         <input
          type="text" 
          onChange={(e) => setInputValue(e.target.value)}
          value={inputValue}
-         onKeyPress={(e) =>{
+         onKeyDown={(e) =>{
           if(e.key === "Enter" && inputValue.trim() != "") {
            addTodos();
           }
         
         }}
-          placeholder="What do you need to do ?"></input></li>
-        {todos.map((item,index) => (
-          <li key={index}>{item && item.label}<button className="hover-button" onClick={() => 
-            setTodos(
-              todos.filter(
-                (t, currentIndex) => 
-                  index != currentIndex
-              )
-            )
-              }>X</button>
-              </li>
+          placeholder="What do you need to do ?">
+            </input>
+          </li>
+        {todos.map((item) => (
+          <li key={item.id}>{item && item.label}<button className="hover-button" onClick={() => 
+              deleteTodo(item.id)}> X </button>
+            </li>
         ))}  
          
       </ul>
